bf3ebacc0b9755b621a79dabd55ee0fac834063d
Micke Carlsson <mickecamino@gmail.com>
2008-09-11 19:57:17 +0000

    Enclosed text strings for localization in pbdirectory

--- install.php
+++ install.php
@@ -1,8 +1,9 @@
 <?php
 
 // Enable phonebook directory as a feature code
+$phonebookdbnd = _("Phonebook dial-by-name directory");
 $fcc = new featurecode('pbdirectory', 'app-pbdirectory');
-$fcc->setDescription('Phonebook dial-by-name directory');
+$fcc->setDescription($phonebookdbnd);
 $fcc->setDefault('411');
 $fcc->update();
 unset($fcc);
